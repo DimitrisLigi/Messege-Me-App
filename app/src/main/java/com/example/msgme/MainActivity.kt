304e1ca9ea2115562e72b8fa0754dc7cd69c843d
Dimitris <dimitrisligi@gmail.com>
2020-07-11 13:52:12 +0300

    All views Ready Commit

--- app/src/main/java/com/example/msgme/MainActivity.kt
+++ app/src/main/java/com/example/msgme/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.msgme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //Hello
+        buttonManager()
+    }
+
+    private fun buttonManager(){
+        tv_subcribe_here.setOnClickListener {
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/msgme/RegisterActivity.kt
+++ app/src/main/java/com/example/msgme/RegisterActivity.kt
@@ -1,11 +1,22 @@
 package com.example.msgme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        buttonsManager()
+    }
+
+    private fun buttonsManager(){
+        tv_already_an_account.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
